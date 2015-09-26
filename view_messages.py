73dcceb10a9f99547cde992659e6d6ca245d73b6
cclauss <cclauss@bluewin.ch>
2015-09-26 06:03:43 +0200

    Minor simplifications

--- view_messages.py
+++ view_messages.py
@@ -6,17 +6,14 @@ class MessagesController(object):
 	def __init__(self):
 		self.view = ui.load_view('view_messages.pyui')
 		self.bt_set_mode(None)
-		modesc = self.view['mode_segmentedcontrol']
-		modesc.action = self.bt_set_mode
+		self.view['mode_segmentedcontrol'].action = self.bt_set_mode
 	
 	def bt_set_mode(self,sender):
 		modesc = self.view['mode_segmentedcontrol']
-		idx = modesc.selected_index
-		if idx > -1:
-			self.mode = modesc.segments[idx]
-		else:
+		try:
+			self.mode = modesc.segments[modesc.selected_index]
+		except IndexError:
 			self.mode = None
 
 if __name__ == '__main__':  # pragma: no cover
-	my_app = MessagesController()
-	my_app.view.present(hide_title_bar=True)
+	MessagesController().view.present(hide_title_bar=True)
